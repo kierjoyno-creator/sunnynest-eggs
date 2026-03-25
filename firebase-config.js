2f01ed50cdf70fb27984fce783bc331436761187
kierjoyno-creator <258136233+kierjoyno-creator@users.noreply.github.com>
2026-03-25 15:27:52 +0800

    Set up new Firebase project and GitHub deploys

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDMGCbRkpAF1XlO3EYXUXGxjNeccBQvMS8",
-  authDomain: "lamyani-a34af.firebaseapp.com",
-  projectId: "lamyani-a34af",
-  storageBucket: "lamyani-a34af.firebasestorage.app",
-  messagingSenderId: "534705321076",
-  appId: "1:534705321076:web:c768702f5561de9a670d21",
-  measurementId: "G-WT17LDB7VJ",
+  apiKey: "AIzaSyCRtKp24h_hOyEfxv59WvVWg94N7pSZoZE",
+  authDomain: "sunnynest-eggs-store.firebaseapp.com",
+  projectId: "sunnynest-eggs-store",
+  storageBucket: "sunnynest-eggs-store.firebasestorage.app",
+  messagingSenderId: "72330625461",
+  appId: "1:72330625461:web:e8257837fefdfa8e8fd9fc",
 };
 
 export default firebaseConfig;
